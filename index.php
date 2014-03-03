f6bceff5301f109befdd056062ef10b6e63aa003
somwang038 <somwang038@hotmail.com>
2014-03-03 22:53:23 +0700

    ทดลองใช้ Bootstrap

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+        include 'header.php';
+?>
+
+<style type="text/css">
+  body {
+    padding-top: 50px;
+    padding-bottom: 20px;
+  }
+</style>
+
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Test Bootstrap</a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <form class="navbar-form navbar-right" role="form">
+        <div class="form-group">
+          <input type="text" placeholder="อีเมล์" class="form-control">
+        </div>
+        <div class="form-group">
+          <input type="password" placeholder="รหัสผ่าน" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-success">เข้าสู่ระบบ</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+<div class="jumbotron">
+  <div class="container">
+    <h1>สวัสดี, ชาวโลก!</h1>
+    <p>นี่เป็นการฝึกทดลองเขียนเว็บโดยใช้ Bootstrap หวังว่าการฝึกฝนครั้งนี้จะไม่มีอุปสรรคเข้ามายุ่งเกี่ยว และขอให้ชาวโลกทั้งหลายช่วยเป็นกำลังใจด้วยนะจ๊ะ</p>
+    <p><a class="btn btn-primary btn-lg" role="button">อ่านเพิ่มเติม »</a></p>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+      <h2>อัลเบิร์ต ไอน์สไตน์</h2>
+      <p>เมื่อพูดถึงชื่อของ อัลเบิร์ต ไอน์สไตน์ แล้ว แน่นอนว่าคงไม่มีใครไม่รู้จักหรือไม่เคยได้ยินชื่อของเขาเป็นแน่ เพราะเขาเป็นอัจฉริยะระดับโลก ที่โด่งดังทั้งในวงการวิทยาศาสตร์ คณิตศาสตร์ และยังเป็นนักคิดค้นที่ไม่เคยหยุดเรียนรู้ค้นหาเลยตลอดช่วงชีวิต 76 ปีของเขา และด้วยเหตุนี้เอง ที่ทำให้ไอน์สไตน์มีผลงานที่เป็นคุณประโยชน์ต่อโลกใบนี้มากมาย อีกทั้งความคิดของเขาก็ถูกเรียงร้อยออกมาเป็นถ้อยคำถ้อยอักษร ชวนให้คนรุ่นหลังได้คิดตามและนำไปใช้ในการดำเนินชีวิตอีกด้วย</p>
+      <p><a class="btn btn-default" href="#" role="button">รายละเอียด »</a></p>
+    </div>
+    <div class="col-md-4">
+      <h2>ไอแซก นิวตัน</h2>
+      <p>เซอร์ไอแซค นิวตัน เกิดเมื่อวันที่ 25 ธันวาคม 1642 ที่ลินคอล์นเชียร์ อังกฤษมในสมัยที่เป็นเด็กไม่ค่อยสนใจในการเรียนนัก ชอบทางด้านเครื่องจักร เครื่องกล แต่พออายุได้ 15 ปี เขากลับเอาใจใส่การศึกษามากขึ้น พอบิดาของเขาถึงแก่กรรมลง มารดาก็ตั้งใจจะให้เขาทำงานในฟาร์มเหมือนบิดา แต่เขาไม่ชอบ นิวตันเป็นคนไม่ชอบเพื่อน</p>
+      <p><a class="btn btn-default" href="#" role="button">รายละเอียด »</a></p>
+   </div>
+    <div class="col-md-4">
+      <h2>กาลิเลโอ กาลิเลอี</h2>
+      <p>กาลิเลโอ กาลิเลอี (Galileo Galilei) เกิด ณ เมืองปิซา ประเทศอิตาลี ในวันที่ 15 กุมภาพันธ์ พ.ศ. 2107 (ค.ศ. 1564) – เสียชีวิต ณ เมืองอาร์เซทิ (Arcetri) ฟลอเรนซ์ ในวันที่ 8 มกราคม พ.ศ. 2185 (ค.ศ. 1642) มีบิดาชื่อ วินเซนซิโอ กาลิเลอิ ซึ่งมีอาชีพเป็นนักดนตรี และมีมารดาชื่อ จูเลีย กาลิเลอิ กาลิเลโอ เป็นนักวิทยาศาสตร์ และยัง ถูกขนานนามในชื่อต่างๆ เช่น "father of modern astronomy" (บิดาแห่งวิชาดาราศาสตร์สมัยใหม่), "father of modern physics" (บิดาแห่งวิชาฟิสิกส์สมัยใหม่) หรือ "father of science" (บิดาแห่งวิทยาศาสตร์) เป็นผู้ค้นพบ ทฤษฎีทางวิทยาศาสตร์มากมาย เช่น "กฏแห่งการแกว่งของลูกตุ้ม" และ "กฏการตกของวัตถุ</p>
+      <p><a class="btn btn-default" href="#" role="button">รายละเอียด »</a></p>
+    </div>
+  </div>
+
+  <hr>
+
+  <footer>
+    <p>© ทดลองเขียนจร้า 2014</p>
+  </footer>
+</div>
+
+<script src="bootstrap/js/jquery-1.11.0.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
